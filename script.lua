9d302959485492fbf6a52be731a79b3988f0166f
XiaoFenHG <2178493675@qq.com>
2024-10-26 18:52:16 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1491,6 +1491,75 @@ Toggles.hi:OnChanged(function(value)
     end
 end)
 a:AddToggle('No Clip', {
+    Text = 'TimeLever aura',
+    Default = false,
+    Tooltip = 'Walk through walls',
+    Callback = function(state)
+        if state then
+            -- open
+            daura = true
+
+            -- getplayer
+            local player = game.Players.LocalPlayer
+
+            -- check
+            workspace.CurrentRooms.ChildAdded:Connect(function(room)
+                room.DescendantAdded:Connect(function(descendant)
+                    if descendant:IsA("Model") then
+                        local prompt = nil
+                        if descendant.Name == "TimerLever" then
+                            prompt = descendant:WaitForChild("ActivateEventPrompt")
+			end
+
+                        if prompt then
+                            local interactions = prompt:GetAttribute("Interactions")
+                            if not interactions then
+                                task.spawn(function()
+                                    while autoInteract and not prompt:GetAttribute("Interactions") do
+                                        task.wait(0.1)
+                                        if player:DistanceFromCharacter(descendant.PrimaryPart.Position) <= 12 then
+                                            fireproximityprompt(prompt)
+                                        end
+                                    end
+                                end)
+                            end
+                        end
+                    end
+                end)
+            end)
+
+            -- check2
+            for _, room in pairs(workspace.CurrentRooms:GetChildren()) do
+                for _, descendant in pairs(room:GetDescendants()) do
+                    if descendant:IsA("Model") then
+                        local prompt = nil
+                        if descendant.Name == "TimerLever" then
+                            prompt = descendant:WaitForChild("ActivateEventPrompt")
+			end
+
+                        if prompt then
+                            local interactions = prompt:GetAttribute("Interactions")
+                            if not interactions then
+                                task.spawn(function()
+                                    while autoInteract and not prompt:GetAttribute("Interactions") do
+                                        task.wait(0.1)
+                                        if player:DistanceFromCharacter(descendant.PrimaryPart.Position) <= 12 then
+                                            fireproximityprompt(prompt)
+                                        end
+                                    end
+                                end)
+                            end
+                        end
+                    end
+                end
+            end
+        else
+            -- close
+            daura = false
+        end
+    end
+})
+a:AddToggle('No Clip', {
     Text = 'Lever aura',
     Default = false,
     Tooltip = 'Walk through walls',
